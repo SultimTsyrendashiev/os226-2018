18069f72cd3dedb118ecf553a4545224d7a4b46f
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-09-05 00:40:43 +0300

    - implemented string.c

--- libc/string.c
+++ libc/string.c
@@ -9,25 +9,101 @@ static inline __attribute__((noreturn)) void IMPL_ME() {
 }
 
 int strcmp(const char *s1, const char *s2) {
-	IMPL_ME();
+	const char *p1 = s1;
+	const char *p2 = s2;
+
+	char c1, c2;
+
+	while ((c1 = *p1++) == (c2 = *p2++) && c1);
+
+	return c1 - c2;
 }
 
 size_t strlen(const char *s) {
-	IMPL_ME();
+	const char *p = s;
+
+	while (*p++);
+
+	return (size_t)(p - s - 1);
 }
 
 char *strchr(const char *s, int c) {
-	IMPL_ME();
+	char ch = (char)c;
+
+	while (*s)
+	{
+		if (*s == ch)
+		{
+			return (char*)s;
+		}
+
+		s++;
+	}
+
+	return NULL;
 }
 
 size_t strspn(const char *str, const char *accept) {
-	IMPL_ME();
+	size_t length = 0;
+
+	while (*str && strchr(accept, *str))
+	{
+		str++;
+		length++;
+	}
+	
+	return length;
 }
 
 size_t strcspn(const char *str, const char *reject) {
-	IMPL_ME();
+	size_t length = 0;
+
+	while (*str)
+	{
+		if (strchr(reject, *str))
+		{
+			return length;
+		}
+		else
+		{
+			str++;
+			length++;
+		}
+	}
+
+	return length;
 }
 
 char *strtok_r(char *str, const char *delim, char **saveptr) {
-	IMPL_ME();
+	
+	if (str == NULL)
+	{
+		str = *saveptr;
+	}
+
+	if (*str == '\0')
+	{
+		*saveptr = str;
+		return NULL;
+	}
+
+	str += strspn(str, delim);
+	if (*str == '\0')
+	{
+		*saveptr = str;
+		return NULL;
+	}
+
+	// end of token
+	char *end = str + strcspn(str, delim);
+	if (*end == '\0')
+	{
+		*saveptr = end;
+		return str;
+	}
+
+	*end = '\0';
+	*saveptr = end + 1;
+
+	return str;
 }
