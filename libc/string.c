6c0e6da36e9e9fe637f89b80e103f23232e479e1
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-09-16 17:59:48 +0300

    - updated string.c

--- libc/string.c
+++ libc/string.c
@@ -3,101 +3,90 @@
 #include <string.h>
 
 int strcmp(const char *s1, const char *s2) {
-	const char *p1 = s1;
-	const char *p2 = s2;
-
-	char c1, c2;
-
-	while ((c1 = *p1++) == (c2 = *p2++) && c1);
-
-	return c1 - c2;
+	while (*s1 && *s1 == *s2) {
+		++s1;
+		++s2;
+	}
+	return *s1 - *s2;
 }
 
 size_t strlen(const char *s) {
 	const char *p = s;
-
-	while (*p++);
-
-	return (size_t)(p - s - 1);
+	while (*p) {
+		++p;
+	}
+	return p - s;
 }
 
 char *strchr(const char *s, int c) {
-	char ch = (char)c;
-
-	while (*s)
-	{
-		if (*s == ch)
-		{
-			return (char*)s;
+	for (const char *p = s; *p != '\0'; ++p) {
+		if (*p == c) {
+			return (char *) p;
 		}
-
-		s++;
 	}
-
 	return NULL;
 }
 
 size_t strspn(const char *str, const char *accept) {
-	size_t length = 0;
-
-	while (*str && strchr(accept, *str))
-	{
-		str++;
-		length++;
+	const char *s = str;
+	while (*s != '\0'  && strchr(accept, *s)) {
+		++s;
 	}
-	
-	return length;
+	return s - str;
 }
 
+
 size_t strcspn(const char *str, const char *reject) {
-	size_t length = 0;
-
-	while (*str)
-	{
-		if (strchr(reject, *str))
-		{
-			return length;
-		}
-		else
-		{
-			str++;
-			length++;
-		}
+	const char *s = str;
+	while (*s != '\0'  && !strchr(reject, *s)) {
+		++s;
 	}
-
-	return length;
+	return s - str;
 }
 
 char *strtok_r(char *str, const char *delim, char **saveptr) {
-	
-	if (str == NULL)
-	{
-		str = *saveptr;
-	}
-
-	if (*str == '\0')
-	{
-		*saveptr = str;
+	char *s = str ? str : *saveptr;
+	s += strspn(s, delim);
+	if (*s == '\0') {
+		*saveptr = s;
 		return NULL;
 	}
 
-	str += strspn(str, delim);
-	if (*str == '\0')
-	{
-		*saveptr = str;
-		return NULL;
+	char *e = s + strcspn(s, delim);
+	if (*e == '\0') {
+		*saveptr = e;
+	} else {
+		*e = '\0';
+		*saveptr = e + 1;
 	}
+	return s;
+}
 
-	// end of token
-	char *end = str + strcspn(str, delim);
-	if (*end == '\0')
-	{
-		*saveptr = end;
-		return str;
+void *memset(void *s, int c, size_t n) {
+	char *e = (char *)s + n;
+	for (char *p = s; p < e; ++p) {
+		*p = c;
 	}
+	return s;
+}
 
-	*end = '\0';
-	*saveptr = end + 1;
+void *memcpy(void *dest, const void *src, size_t n) {
+	const char *e = src + n;
+	const char *s = src;
+	char *d = dest;
+	while (s < e) {
+		*d++ = *s++;
+	}
+	return dest;
+}
 
-	return str;
+int memcmp(const void *s1, const void *s2, size_t n) {
+	const char *ss1 = s1;
+	const char *ss2 = s2;
+	for (int i = 0; i < n; ++i) {
+		if (ss1[i] != ss2[i]) {
+			return ss1[i] - ss2[i];
+		}
+	}
+	return 0;
 }
