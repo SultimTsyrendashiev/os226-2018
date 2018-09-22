ef0b7fe7f7c96d93d285bee8fcf521eb4c195ee1
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-09-22 23:49:18 +0300

    -

--- kernel/init.c
+++ kernel/init.c
@@ -11,7 +11,6 @@ void kernel_init(void *rootfs_cpio, void *mem, size_t sz, const char* args) {
 	kernel_globals.rootfs_cpio = rootfs_cpio;
 	kernel_globals.mem = mem;
 	kernel_globals.memsz = sz;
-	kernel_globals.stackPtr = 0;
 }
 
 void kernel_start(void) {
