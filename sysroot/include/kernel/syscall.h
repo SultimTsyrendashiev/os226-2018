bf1421e156d68d2a1a19a78ae60a95f3e3a54037
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-09-30 22:17:47 +0300

    - added sys_push_task: returns int, receives char** argv

--- sysroot/include/kernel/syscall.h
+++ sysroot/include/kernel/syscall.h
@@ -10,6 +10,7 @@ typedef void (*tramp_t)(void *arg);
 	x(exit, int, 1, int, exitcode) \
 	x(write, int, 3, int, fd, const void *, buf, size_t, len) \
 	x(read, int, 3, int, fd, void *, buffer, size_t, size) \
+	x(push_task, int, 1, char**, argv) \
 
 #define ENUM_LIST(name, ...) os_syscall_nr_ ## name,
 enum syscalls_num {
